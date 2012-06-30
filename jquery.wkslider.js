2afd52820bcb3d1f3483566adda7292918ebe11b
Daniele Orler <daniele.orler@gmail.com>
2012-07-01 00:39:58 +0200

    fix #2 and #3
    #3
    When the slider is not visible (user switched to another browser tab)
    the slider is stopped
    #2 refactored codebase to adhere to a simple design pattern suggested
    by Addy Osmany
    (http://coding.smashingmagazine.com/2011/10/11/essential-jquery-plugin-p
    atterns/)

--- jquery.wkslider.js
+++ jquery.wkslider.js
@@ -1,102 +1,144 @@
-(function($)
+/*
+ * plugin's template taken from Addy Osmani's http://coding.smashingmagazine.com/2011/10/11/essential-jquery-plugin-patterns/
+ */
+
+;(function($, window, document, undefined)
 {
-    $.fn.wkslider = function(options)
+    //plugin's name
+    var pluginName = "wkslider";
+    //defaults    
+    var defaults =
     {
-        //store 'this' element for scoping
-        var container = this;
-        var divs = this.children('div');    	
-        //numbers of slides
-        var n = divs.length;
-        
-        //if there are less than 2 slides there is no need of the slider
-    	if(n < 2)
-    		return this;
-    	
-        //defaults    
-        var defaults =
+        slideElement    : 'div',
+        menuClass       : 'slider_menu',
+        selector        : 'o',
+        autoplay        : false,
+        activate        : 'active',
+        time            : 3000,
+        effect          : 'base'
+    }
+
+    //plugin contructor
+    function Plugin(element,options)
+    {
+        this.container      = $(element);
+        this.options        = $.extend({},defaults, options);
+        this.slides         = this.container.children(this.options.slideElement);
+        this.slidesNumber   = this.slides.length;
+
+        //transaction effects
+        this.effects        =
         {
-        	css		: 'slider_menu',
-            selector: 'o',
-            autoplay: false,
-            activate: 'active',
-            time	: 3000
-        }
-        var opts = $.extend(defaults, options);
-                
-        //move the 'highlight' to active selector
-        this.move_selector = function(i)
-        {
-            ul.children('li').removeClass(opts.activate).eq(i|0).addClass(opts.activate);
-        };
-        
-        this.move_slide = function(i)
-        {
-            divs.filter(':visible').fadeOut('slow',function()
+            base     : function(slides,i)
             {
-                divs.filter(':visible').hide();
-                divs.eq(i).fadeIn('slow');
-            });              
-        }
-        
-        this.get_active_index = function()
-        {
-            return ul.find('.'+opts.activate).index();
-        };
-        
-        this.autoslide = function()
-        {
-            var start = container.get_active_index();
-            var step = (start+1)%n;
-            container.move_selector(step);
-            container.move_slide(step);
-        }
-                
-        //display just the first slide
-        divs.hide().eq(0).show();
-                
-        //create slider's menu container
-        var ul = $(document.createElement('ul'));
-                
-        //create actual menu
-        for(var i = 0; i < n; i++)
-        {
-            var li = $(document.createElement('li'));
-                    
-            switch(opts.selector)
+                slides.filter(':visible').fadeOut('slow',function()
+                {
+                    slides.filter(':visible').hide();
+                    slides.eq(i).fadeIn('slow');
+                });
+            },
+            orizontal: function(slides,i)
             {
-                case 'number':        li.text(i+1); break;
-                case 'character':    li.text(String.fromCharCode(97+i)); break;
-                default:            li.text(opts.selector);
+                slides.filter(':visible').hide('slide',{direction:'left'},1000);
+                slides.eq(i).show('slide',{direction:'right'},1000);
             }
-                    
-            ul.append(li);
-        }
-                
-        if(typeof opts.css !== 'undefined')
-            ul.addClass(opts.css);
-                
-        this.move_selector();
-                
-        this.append(ul);
-                
-        ul.children('li').bind('click',function()
-        {
-            var index = $(this).index();
-            container.move_selector(index);
-            container.move_slide(index);
-            window.clearInterval(container.intervals);
-        });
-        
-        if(opts.autoplay)
-        {
-        	container.intervals = window.setInterval(container.autoslide,opts.time);
         }
 
-        $(window).unload(function()
-        {
-            clearInterval(container.intervals);
-            container.intervals = null;
-        });
-                
+        this.init();
     }
-})(jQuery);
+
+    Plugin.prototype.init = function()
+    {
+        //if there are less than 2 slides there is no need for the slider
+        if(this.slidesNumber < 2)
+        {
+            return this.container;
+        }
+        else
+        {
+            //display just the first slide
+            this.slides.hide().eq(0).show();
+            //create slider's menu container
+            this.sliderMenu = $(document.createElement('ul'));
+            //create actual menu
+            for(var i = 0; i < this.slidesNumber; i++)
+            {
+                var li = $(document.createElement('li'));
+
+                switch(this.options.selector)
+                {
+                    case 'number'       : li.text(i+1); break;
+                    case 'character'    : li.text(String.fromCharCode(97+i)); break;
+                    default             : li.text(this.options.selector);
+                }
+
+                this.sliderMenu.append(li);
+            }
+
+            if(typeof this.options.menuClass !== 'undefined')
+                this.sliderMenu.addClass(this.options.menuClass);
+
+            this.moveSelector();
+            this.container.append(this.sliderMenu);
+
+            var container = this;
+            this.sliderMenu.children('li').on('click',function()
+            {
+                var index = $(this).index();
+                container.moveSelector(index);
+                container.moveSlide(index);
+                window.clearInterval(container.intervals);
+            });
+
+            if(this.options.autoplay)
+            {
+                var that = this;
+                this.intervals = window.setInterval(function(){that.autoslide();},this.options.time);
+            }
+
+            //when the slider is not visible the stop it
+            $(window).bind('blur',function()
+            {
+                clearInterval(container.intervals);
+            });
+        }
+    };
+    	
+    //move the 'highlight' to active selector
+    Plugin.prototype.moveSelector = function(i)
+    {
+        this.sliderMenu.children('li').removeClass(this.options.activate).eq(i|0).addClass(this.options.activate);
+    };
+
+    Plugin.prototype.moveSlide = function(i)
+    {
+        var slides = this.slides;
+        this.effects[this.options.effect](slides,i);
+    }
+        
+    Plugin.prototype.getActiveIndex = function()
+    {
+        return this.sliderMenu.find('.'+this.options.activate).index();
+    };
+
+    Plugin.prototype.autoslide = function()
+    {
+        var start = this.getActiveIndex();
+        var step = (start+1)%this.slidesNumber;
+        this.moveSelector(step);
+        this.moveSlide(step);
+    };
+
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options)
+    {
+        return this.each(function()
+        {
+            if (!$.data(this, 'plugin_' + pluginName))
+            {
+                $.data(this,'plugin_' + pluginName, new Plugin(this, options));
+            }
+        });
+    }
+})( jQuery, window, document );
